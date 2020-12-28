8be961deb7eeb75949a4f3cca4832f79f3c30995
Souvik <souvikat001@gmail.com>
2020-12-28 20:27:41 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import './App.css';
-import './css/footer.css'
+// import './App.css';
+import './css/footer.css';
+import './css/navbar.css';
 import BackgroundClipped from './components/BackgroundClipped';
 import FooterLanding from './components/FooterLanding';
 import BackgroundBlurred from './components/BackgroundBlurred';
 import LandingBody from './components/LandingBody';
-// import ProjectSection from './components/ProjectSection';
-import RegistrationForm from './components/RegistrationForm'
+import Navbar from './components/navbar';
+import {  BrowserRouter, Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
        <div>
+          <Navbar></Navbar>
           <BackgroundClipped></BackgroundClipped>
-          <LandingBody></LandingBody>
-          {/* The Below Form Will be Linked with Events Section */}
-          {/* <RegistrationForm></RegistrationForm> */}
+          <BrowserRouter>
+            <Switch>
+              <Route path="/home" component={LandingBody} />
+              <Route path="/" exact component={LandingBody} />
+              <Route render={() => <h1>404: page not found</h1>} />
+            </Switch>
+          </BrowserRouter>
           <FooterLanding></FooterLanding>
        </div>
     );
