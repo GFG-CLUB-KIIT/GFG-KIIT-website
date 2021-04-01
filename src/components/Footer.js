f3f56e083f857f80165a7d37ac836962f29e24c9
AnkitRaj <1906534@kiit.ac.in>
2021-04-01 22:44:16 +0530

    member page and footer page issue resolve

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import '../css/footer.css'
-
-function FooterLanding(props) {
-   return (
-      <div class="parent-element">
+function FooterLanding(props){
+   return ( 
+      <div>
          <div className={`footer ${props.bgColor}`}>
             <div class="grid-container">
                <div class="wanna-talk-to-us"><h1>Wanna talk to us?</h1></div>
@@ -11,6 +10,7 @@ function FooterLanding(props) {
                      <form id="contact-us">
                         <input type="email" id="email" name="email" placeholder="Email"></input>
                         <textarea type="text" id="message" name="messsage" placeholder="Start typing!"></textarea>
+                        {/* TODO: style the button */}
                         <div className="send-btn-container"><input type="submit" value="Send" className="send"></input></div>
                      </form>
                   </div>
@@ -28,14 +28,35 @@ function FooterLanding(props) {
                         <div class="description1">
                            <p class = "contact strong">Feel free to contact us</p>
                            <br></br>
-                           <p>GFG KIIT</p>
+                           <p>School of Computer</p>
+                           <p>Engineering, KIIT</p>
+                           <p>University, Bhubaneswar</p>
+                           <p>751024</p>
+                           <p>Email: gfgkiit@gmail.com</p>
+                           <p>Phone: +91 87655 16718</p>
+                           <p>(Taran)</p>
                            <br></br>
+                           </div>
+                        <div>
+                           <div class="description3">
+                              <p class = "guideline strong">General Guidelines</p>
+                              <br></br>
+                              <p>Code of Conduct</p>    
+                              <br></br>        
+                              <br></br>        
+                           </div>
+                           <div class="description2">
+                              <p class = "community strong">Community</p>
+                              <br></br>
+                              <p>GFG KIIT</p>
+                              <br></br>
                         </div>
                      </div>
                   </div>
                </div>
-            </div>
+            </div>   
          </div>
+      </div>
 
    )
 }
