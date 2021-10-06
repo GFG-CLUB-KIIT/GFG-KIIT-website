ca69abf11102410075c87eab8559dc68f250ff50
OmPr366 <71120474+OmPr366@users.noreply.github.com>
2021-10-07 03:32:08 +0530

    Dark mode  Issue fixed

--- src/components/ToggleSwitch.js
+++ src/components/ToggleSwitch.js
@@ -1,30 +1,39 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 // import './App.css';
-import { ThemeProvider } from 'styled-components';
-import { lightTheme, darkTheme } from '../components/Themes';
-import { GlobalStyles } from '../components/Global';
-
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../components/Themes";
+import { GlobalStyles } from "../components/Global";
 
 export const Toggle = () => {
+  const [theme, setTheme] = useState("light");
+  
 
-  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    setTheme(x)
+    if (theme === "light") {
+      localStorage.setItem("myGfgTheme","dark")
+      setTheme("dark");
+    } else   {
+      localStorage.setItem("myGfgTheme","light")
+      setTheme("light");
+    }
+    
+  };
+  
 
-const toggleTheme = () => {
-  if (theme === 'light') {
-    setTheme('dark');
-  } else {
-    setTheme('light');
-  }
-}
+  let x = localStorage.getItem("myGfgTheme")? localStorage.getItem("myGfgTheme"):"light";
+
   return (
-
-    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+    // <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+    <ThemeProvider theme={x=="light"? lightTheme:darkTheme}>
       <>
         <GlobalStyles />
         <label class="switch">
-          <input type="checkbox" onClick={toggleTheme} ></input>
-          <span class="slider round"></span>
+          {/* <input type="checkbox" onClick={toggleTheme}></input>
+          <span class="slider round"></span> */}
+          <box-icon type='solid' color ={x==='dark'?'#e9e9e2':'#060606'} onClick={toggleTheme}  name={x=='dark'?'sun':'moon'} class="themeIcon"></box-icon>
         </label>
+        
 
         {/* <button className='btn' onClick={toggleTheme}>O</button> /}
         {/ <Toggle theme={theme} toggleTheme={toggleTheme} /> /}
@@ -32,6 +41,6 @@ const toggleTheme = () => {
       </>
     </ThemeProvider>
   );
-}
+};
 
 export default Toggle;
