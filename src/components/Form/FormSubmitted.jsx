a1dc32d58a05f12f49a5f86676bf19250f4a05e3
Om Prakash <71120474+OmPr366@users.noreply.github.com>
2022-01-29 22:59:18 +0530

    JSON Added

--- src/components/Form/FormSubmitted.jsx
+++ src/components/Form/FormSubmitted.jsx
@@ -1,10 +1,27 @@
-import React from "react";
-import loadingGif from "../../img/form-submitted-gif.gif";
+import React,{useRef} from "react";
+import lottie from 'lottie-web'
 const FormSubmitted = () => {
+
+  // Lottie Files for IS Submit
+  
+  const container = useRef(null); 
+  const [firstLoad, setFirstLoad] = React.useState(1);
+
+  React.useEffect(() => {
+    lottie.loadAnimation(
+      {
+        container: container.current,
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        animationData: require("../../json/Submit.json"),
+      }
+    );
+  },[firstLoad]);
   return (
     <>
       <div className="form-submitted">
-        <img className="image-unselectable" width="350px" src={loadingGif} />
+        <div className="submitGif" ref={container}></div>
         <h2 className="aboutUs-slogan">
           Form submitted sucessfully, you can close this window now!
         </h2>
